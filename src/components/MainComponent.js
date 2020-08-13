951d8baf080c19eb03c242ed260fd07e04ebf5b7
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-08-13 15:01:04 -0500

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
@@ -50,6 +51,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact}/>
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
               <Redirect to="/home" />
           </Switch>
         <Footer />

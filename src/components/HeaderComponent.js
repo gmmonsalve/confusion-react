b1fdee79b955841a0e975074cc8e76b7390b2246
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-08-26 17:21:53 -0500

    isModalOpen fixed

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -22,7 +22,7 @@ class Header extends Component {
       }
       toggleModal() {
         this.setState({
-          isModalOpen: !this.state.isNavOpen
+          isModalOpen: !this.state.isModalOpen
         });
       }
 

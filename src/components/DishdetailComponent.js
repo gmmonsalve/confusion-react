ee6123e41e637d696bf726b9e2469e5255dfd0dc
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-09-12 22:42:40 -0500

    animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -119,6 +120,10 @@ class CommentForm extends Component{
 function RenderDish({dish})  {
         if (dish != null)
             return(
+                <FadeTransform in 
+                transformProps={{
+                    exitTransform : 'scale(0.5) translateY(-50%)'
+                }}>
                 <Card>
                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
@@ -126,6 +131,7 @@ function RenderDish({dish})  {
                       <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
+                </FadeTransform>
             );
         else
             return(
@@ -135,16 +141,23 @@ function RenderDish({dish})  {
 
 function RenderComments({comments, postComment, dishId}) {
         if(comments){
+            
         const commts = comments.map((commit)=>{
+           
                 return(
+                    <Stagger in>
                     <div>
+                        <Fade in>
                         <li>{commit.comment}</li>
                         <li>-- {commit.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(commit.date)))}</li>
                         <br></br>
+                        </Fade>
                     </div>
-                        
+                    </Stagger>
                     );
+                   
                 });
+                
                 return(
                     <div>
                         <h4>Comments</h4>
